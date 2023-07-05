0e79f0fdeb1b53edc5d49ae54668a6cd6c67aa61
MatheusOliveira04 <samatheus999@gmail.com>
2023-07-05 10:48:35 -0300

    add css/cadastro2

--- js/cadastro2-js.js
+++ js/cadastro2-js.js
@@ -7,7 +7,9 @@ function formularioIMC(){
         altura : document.getElementById("txtAltura").value
     }
 
-    const msg = `Nome: ${pessoa.nome} \nSexo: ${pessoa.sexo} \nIMC: ${calculaIMC(pessoa.peso, pessoa.altura)}`
+    const imc = calculaIMC(pessoa.peso, pessoa.altura)
+
+    const msg = `Nome: ${pessoa.nome} \nSexo: ${pessoa.sexo} \nIMC: ${imc} \nAvaliação: ${avaliaIMC(imc, pessoa.sexo)}`
 
     document.getElementById("txtDados").value = msg;
 
@@ -15,14 +17,42 @@ function formularioIMC(){
 }
 
 function calculaIMC(peso, altura){
-    return peso / (altura * altura);
+    return peso / ((altura / 100)* (altura / 100));
 }
 
 function avaliaIMC(imc, sexo){
     var masculino = "Masculino";
     if(masculino == sexo){
-        return 
+        if(imc < 20.7){
+            return `Abaixo do peso`
+        }
+        else if(imc >= 20.7 && imc < 26.4){
+            return `Peso normal`
+        } 
+        else if(imc >= 26.4 && imc < 27.8){
+            return `Marginalmente acima do peso` 
+        } 
+        else if(imc >= 27.8 && imc < 31.1){
+            return `Acima do peso ideal`
+        } 
+        else {
+            return `acima do peso`
+        }
     } else{
-
+        if(imc < 19.1){
+            return `Abaixo do peso`
+        }
+        else if(imc >= 19.1 && imc < 25.8){
+            return `Peso normal`
+        } 
+        else if(imc >= 25.8 && imc < 27.30){
+            return `Marginalmente acima do peso` 
+        } 
+        else if(imc >= 27.3 && imc < 32.3){
+            return `Acima do peso ideal`
+        } 
+        else {
+            return `acima do peso`
+        }
     }
 }
